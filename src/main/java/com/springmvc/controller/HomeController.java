ba3d1edcded438a308dc5b0e6c0cc0ca26ebc3d7
Jupiter <kjimin2123@gmail.com>
2025-03-18 10:26:16 +0900

    feat: Enhance HomeController to display server time and update encodings configuration

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
+    public String home(Locale locale, Model model) {
+        Date data = new Date();
+        DateFormat dateformat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateformat.format(data);
+
+        model.addAttribute("serverTime", formattedDate);
+
         return "home";
     }
 }
--- src/main/webapp/views/home.jsp
+++ src/main/webapp/views/home.jsp
@@ -12,5 +12,6 @@
 </head>
 <body>
   <h1>Hello World</h1>
+  <p>The time on the server is ${serverTime}</p>
 </body>
 </html>
