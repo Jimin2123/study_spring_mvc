a3ce85662e0eb09de993f6b24894304af85d9d99
Jupiter <kjimin2123@gmail.com>
2025-03-18 11:23:15 +0900

    feat: Add WelcomeController and welcome2.jsp for home page greeting
    - 부트스트랩 css를 적용하였습니다.

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -13,10 +13,17 @@ import java.util.Locale;
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
+        // Locale = 지역(타임존)
+        // Model = 데이터를 주고받을 수 있도록 한 표준 객체
+
+        // 서버의 시스템 시간
         Date data = new Date();
+        // 날짜 타입을 지정
         DateFormat dateformat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        // 문자열로 변환
         String formattedDate = dateformat.format(data);
 
+        // model 객체에 key:serverTime,value:formattedDate 정보를  추가함.
         model.addAttribute("serverTime", formattedDate);
 
         return "home";
