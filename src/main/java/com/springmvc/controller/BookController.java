f5ba9e9056c92f0ffd29697bf1058a39b0643212
Jupiter <kjimin2123@gmail.com>
2025-03-25 12:27:33 +0900

    4week : 데이터베이스 연동, 로그 사용

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.springmvc.controller;
 import java.util.List;
+
+import org.slf4j.Logger; // slf4j란 : 다양한 로깅 프레임워크에 대한 간단한 퍼사드 또는 추상화 역할
+import org.slf4j.LoggerFactory; // SLF4J API를 사용하여 로깅을 수행하는 클래스
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,11 +13,14 @@ import com.springmvc.service.BookService;
 
 @Controller
 public class BookController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private BookService bookService;
 
     @RequestMapping(value="/books", method=RequestMethod.GET)
     public String requestBookList(Model model) {
+        this.log.debug("requestBookList");
         List<Book> list = bookService.getAllBookList();
         model.addAttribute("bookList", list);
         return "books";
--- src/main/webapp/views/books.jsp
+++ src/main/webapp/views/books.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="jakarta.tags.core"%>
-<%@ taglib prefix="fn" uri="jakarta.tags.functions"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core"%> <!-- c: JSTL Core -->
+<%@ taglib prefix="fn" uri="jakarta.tags.functions"%> <!-- fn: JSTL Functions -->
 <!DOCTYPE html>
 <html>
 <head>
